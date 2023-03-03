9563269bf34f369d9c73fa0c26447caf27a5f3fe
Lokesh S <lokeshlucky13579@gmail.com>
2023-03-03 17:19:30 +0530

    printing only in debug mode

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -12,7 +13,32 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Future<void> getLocation() async {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    if (kDebugMode) {
+      print(position);
+    }
+  }
+
+  Future<void> checkPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
   }
 
   @override
@@ -21,26 +47,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () async {
-            bool serviceEnabled;
-            LocationPermission permission;
-
-            serviceEnabled = await Geolocator.isLocationServiceEnabled();
-            if (!serviceEnabled) {
-              return Future.error('Location services are disabled');
-            }
-
-            permission = await Geolocator.checkPermission();
-            if (permission == LocationPermission.denied) {
-              permission = await Geolocator.requestPermission();
-              if (permission == LocationPermission.denied) {
-                return Future.error('Location permissions are denied');
-              }
-            }
-
-            if (permission == LocationPermission.deniedForever) {
-              return Future.error(
-                  'Location permissions are permanently denied, we cannot request permissions.');
-            }
+            checkPermission();
             getLocation();
           },
           child: const Text('Get Location'),
